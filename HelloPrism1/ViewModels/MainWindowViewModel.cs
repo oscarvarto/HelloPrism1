cec26b62ab4698e947435cdb872ee64e6deee1d1
Oscar Vargas Torres <oscarvarto@protonmail.com>
2019-02-15 18:43:01 -0600

    Initial version of HelloPrism1.

--- HelloPrism1/ViewModels/MainWindowViewModel.cs
+++ HelloPrism1/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,53 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
 
 namespace HelloPrism1.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Hello Prism";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private bool _isEnabled = false;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        private string _updateText = "";
+        public string UpdateText
+        {
+            get => _updateText;
+            set => SetProperty(ref _updateText, value);
+        }
+
+        public DelegateCommand<int?> MyAwesomeCommand
+        {
+            get;
+            private set;
         }
 
         public MainWindowViewModel()
         {
+            MyAwesomeCommand = 
+                new DelegateCommand<int?>(Execute)
+                    .ObservesCanExecute(() => IsEnabled);
+        }
 
+        private void Execute(int? parameter)
+        {
+            if (parameter.HasValue)
+            {
+                UpdateText = $"Received parameter: {parameter}";
+            }
         }
     }
 }
